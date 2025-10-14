6b970c538b524a05d110297cad37fb3d5c4eac02
Kempie84 <67120277+Kempie84@users.noreply.github.com>
2025-10-14 16:02:43 +0200

    add solution to assignment 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num <0){return "ERROR"}
+    let output = "";
+    for(let i = 0; i < num; i++){
+        output = string + output;}
+    return output;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
