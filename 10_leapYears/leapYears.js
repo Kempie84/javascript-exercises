af1b1566a255742b1e8bf22beb0f50ae9ecd5e8b
Kempie84 <67120277+Kempie84@users.noreply.github.com>
2025-10-27 17:00:10 +0100

    simplified solution to assignment 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -2,12 +2,10 @@ const leapYears = function(year) {
     if (year % 4 != 0){
         return false;
     }
-    if (year % 4 == 0){
-        if (year % 100 == 0 && year % 400 != 0){
-            return false;
-        } else {
-            return true;
-        }
+    if (year % 100 == 0 && year % 400 != 0){
+        return false;
+    } else {
+        return true;
     }
 };
 
