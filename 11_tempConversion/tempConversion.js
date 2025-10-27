dc61040b3ff1406bb23216b515de6f48a64016f4
Kempie84 <67120277+Kempie84@users.noreply.github.com>
2025-10-27 19:39:34 +0100

    add solution to assignment 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const roundToOneDecimal = function(number) {
+  let result = 0;
+  result = Math.round(number * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(degreesF) {
+  let degreesC = (degreesF - 32) * (5 / 9);
+  return roundToOneDecimal(degreesC);
+};
+
+const convertToFahrenheit = function(degreesC) {
+  let degreesF = degreesC * (9 / 5) + 32;
+  return roundToOneDecimal(degreesF);
 };
 
 // Do not edit below this line
