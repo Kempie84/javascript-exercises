cc2cc14b1bce50b8d6ff76ada420ef9edeab297a
Kempie84 <67120277+Kempie84@users.noreply.github.com>
2026-01-01 20:12:08 +0100

    add solutions to last assignments

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanString = string
+                        .toLowerCase()
+                        .replace(/[!@#$%^&., ?]/g, "")
+                        .split("");
 
+    console.log(cleanString);
+    return cleanString.join('') === cleanString.reverse().join('');
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    if (input < 0) {
+        return "OOPS";
+    } else if (input == 0) {
+        return 0;
+    };
 
+    let fibonacci = [1, 1];
+    for (let i = 2; i < input; i++) {
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    };
+    console.log(fibonacci);
+    return fibonacci[input-1];
 };
 
 // Do not edit below this line
